5d6748cd0b0d82bca61ab72e523d657d1e45cdee
prathmesh1602 <prathmeshshinde4516@gmail.com>
2024-12-14 17:15:08 +0530

    Addes User Authentication

--- prsproject/prsproject/settings.py
+++ prsproject/prsproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'eapp',
+    'prsauth',
 ]
 
 MIDDLEWARE = [
@@ -121,7 +123,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Points to your static folder
 ]
 
+MESSAGE_TAGS = {
+    messages.ERROR:'danger'
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- prsproject/prsproject/urls.py
+++ prsproject/prsproject/urls.py
@@ -20,4 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('eapp.urls')),
+    path('prsauth/',include('prsauth.urls')),
 ]
