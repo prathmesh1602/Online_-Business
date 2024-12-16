3dd8bd7f1ce755640228791d18f6989652935d04
prathmesh1602 <prathmeshshinde4516@gmail.com>
2024-12-16 21:57:27 +0530

    Email Verification included

--- prsproject/prsproject/settings.py
+++ prsproject/prsproject/settings.py
@@ -126,6 +126,14 @@ STATICFILES_DIRS = [
 MESSAGE_TAGS = {
     messages.ERROR:'danger'
 }
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'munnashinde4889@gmail.com'
+EMAIL_HOST_PASSWORD = 'jkvs pkbb txaa faeb'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
